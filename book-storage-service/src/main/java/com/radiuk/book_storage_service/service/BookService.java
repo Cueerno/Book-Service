2dd2a3a8384f5306d4cef0a81f10119a2f8d9780
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-02-09 20:30:08 +0300

    update project structure, add services interfaces

--- book-storage-service/src/main/java/com/radiuk/book_storage_service/service/BookService.java
+++ book-storage-service/src/main/java/com/radiuk/book_storage_service/service/BookService.java
@@ -1,50 +1,16 @@
 package com.radiuk.book_storage_service.service;
 
+
 import com.radiuk.book_storage_service.model.Book;
-import com.radiuk.book_storage_service.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-@Transactional(readOnly = true)
-public class BookService {
+public interface BookService {
 
-    private final BookRepository bookRepository;
-
-    @Autowired
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
-
-    public List<Book> findAll() {
-        return bookRepository.findAll();
-    }
-
-    public Book findById(int id) {
-        return bookRepository.findById(id).get();
-    }
-
-    public Book findByIsbn(String isbn) {
-        return bookRepository.findByIsbn(isbn).get();
-    }
-
-    @Transactional
-    public void create(Book book) {
-        bookRepository.save(book);
-    }
-
-    @Transactional
-    public void update(Book updatedBook, int id) {
-        updatedBook.setId(id);
-        bookRepository.save(updatedBook);
-    }
-
-//    @Transactional
-//    public void delete(int id) {
-//        bookRepository.deleteById(id);
-//    }
+    List<Book> findAll();
+    Book findById(int id);
+    Book findByIsbn(String isbn);
+    void create(Book book);
+    void update(Book updatedBook, int id);
+    // void delete(int id);
 }
---
+++ book-storage-service/src/main/java/com/radiuk/book_storage_service/service/impl/BookServiceImpl.java
@@ -0,0 +1,51 @@
+package com.radiuk.book_storage_service.service.impl;
+
+import com.radiuk.book_storage_service.model.Book;
+import com.radiuk.book_storage_service.repository.BookRepository;
+import com.radiuk.book_storage_service.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public Book findById(int id) {
+        return bookRepository.findById(id).get();
+    }
+
+    public Book findByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn).get();
+    }
+
+    @Transactional
+    public void create(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Transactional
+    public void update(Book updatedBook, int id) {
+        updatedBook.setId(id);
+        bookRepository.save(updatedBook);
+    }
+
+//    @Transactional
+//    public void delete(int id) {
+//        bookRepository.deleteById(id);
+//    }
+}
