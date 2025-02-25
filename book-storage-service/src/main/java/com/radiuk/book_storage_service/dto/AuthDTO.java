50e7357dba30255f13b33077438d220b392f2ece
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-02-25 14:19:56 +0300

    remove unnecessary code
    - constructors in the AuthDTO

--- book-storage-service/src/main/java/com/radiuk/book_storage_service/dto/AuthDTO.java
+++ book-storage-service/src/main/java/com/radiuk/book_storage_service/dto/AuthDTO.java
@@ -1,14 +1,10 @@
 package com.radiuk.book_storage_service.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthDTO {
 
     private String username;
